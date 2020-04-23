4a899098a89b9635a02e3c820216e150a0420f3b
EdieAprovita <edieveg316@gmail.com>
2020-04-23 18:59:44 -0500

    Conectando la base de datos

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("hello"));
+//Connect MongoDB Database
 
-const port = process.env.PORT || 5000;
+connectDB();
 
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.get("/", (req, res) => res.send("API Running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
