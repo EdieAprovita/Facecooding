01b6c55639a62b431be410ca57ca57746be6c571
EdieAprovita <edieveg316@gmail.com>
2020-04-24 11:25:13 -0500

    Creando rutas para crear y borrar post

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -14,7 +14,7 @@ router.get("/me", auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({
       user: req.user.id,
-    }).populate("user", ["username", "avatar"]);
+    }).populate("user", ["name", "avatar"]);
 
     if (!profile) {
       return res
